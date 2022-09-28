ad985c343b99097dc7b01d7cab804893836b6b8c
MunnaGupta17 <mgabd17@gmail.com>
2022-09-29 02:14:35 +0530

    bean classes added

--- src/com/redBus/bean/Bus.java
+++ src/com/redBus/bean/Bus.java
@@ -3,75 +3,142 @@ package com.redBus.bean;
 import java.time.LocalDateTime;
 
 public class Bus {
-	
+
 	private String name;
-	private String busNo;
-	private String bus_Route;
+	private String number;
+	private int driverId;
+	private String route;
 	private String type;
-	private LocalDateTime busArriavl;
-	private LocalDateTime busDeparture;
-	private int total_Seat;
-	public Bus(String name, String busNo, String bus_Route, String type, LocalDateTime busArriavl,
-			LocalDateTime busDeparture, int total_Seat) {
+	private String source;
+	private String destination;
+	private LocalDateTime arrivalTime;
+	private LocalDateTime departureTime;
+	private int totalSeats;
+	private int availableSeats;
+	private boolean isAvailable;
+	private int price;
+
+	public Bus(String name, String number, String route, String type, LocalDateTime arrivalTime,
+			LocalDateTime departureTime, int totalSeats, int availableSeats) {
 		super();
 		this.name = name;
-		this.busNo = busNo;
-		this.bus_Route = bus_Route;
+		this.number = number;
+		this.route = route;
 		this.type = type;
-		this.busArriavl = busArriavl;
-		this.busDeparture = busDeparture;
-		this.total_Seat = total_Seat;
+		this.arrivalTime = arrivalTime;
+		this.departureTime = departureTime;
+		this.totalSeats = totalSeats;
+		this.availableSeats = availableSeats;
 	}
+
+	public int getDriverId() {
+		return driverId;
+	}
+
+	public void setDriverId(int driverId) {
+		this.driverId = driverId;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public boolean isAvailable() {
+		return isAvailable;
+	}
+
+	public void setAvailable(boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getBusNo() {
-		return busNo;
+
+	public String getNumber() {
+		return number;
 	}
-	public void setBusNo(String busNo) {
-		this.busNo = busNo;
+
+	public void setNumber(String number) {
+		this.number = number;
 	}
-	public String getBus_Route() {
-		return bus_Route;
+
+	public String getRoute() {
+		return route;
 	}
-	public void setBus_Route(String bus_Route) {
-		this.bus_Route = bus_Route;
+
+	public void setRoute(String route) {
+		this.route = route;
 	}
+
 	public String getType() {
 		return type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
-	public LocalDateTime getBusArriavl() {
-		return busArriavl;
+
+	public LocalDateTime getArrivalTime() {
+		return arrivalTime;
 	}
-	public void setBusArriavl(LocalDateTime busArriavl) {
-		this.busArriavl = busArriavl;
+
+	public void setArrivalTime(LocalDateTime arrivalTime) {
+		this.arrivalTime = arrivalTime;
 	}
-	public LocalDateTime getBusDeparture() {
-		return busDeparture;
+
+	public LocalDateTime getDepartureTime() {
+		return departureTime;
 	}
-	public void setBusDeparture(LocalDateTime busDeparture) {
-		this.busDeparture = busDeparture;
+
+	public void setDepartureTime(LocalDateTime departureTime) {
+		this.departureTime = departureTime;
 	}
-	public int getTotal_Seat() {
-		return total_Seat;
+
+	public int getTotalSeats() {
+		return totalSeats;
 	}
-	public void setTotal_Seat(int total_Seat) {
-		this.total_Seat = total_Seat;
+
+	public void setTotalSeats(int totalSeats) {
+		this.totalSeats = totalSeats;
 	}
+
+	public int getAvailableSeats() {
+		return availableSeats;
+	}
+
+	public void setAvailableSeats(int availableSeats) {
+		this.availableSeats = availableSeats;
+	}
+
 	@Override
 	public String toString() {
-		return "Bus [name=" + name + ", busNo=" + busNo + ", bus_Route=" + bus_Route + ", type=" + type
-				+ ", busArriavl=" + busArriavl + ", busDeparture=" + busDeparture + ", total_Seat=" + total_Seat + "]";
+		return "Bus [name=" + name + ", busNo=" + number + ", bus_Route=" + route + ", type=" + type + ", busArriavl="
+				+ arrivalTime + ", busDeparture=" + departureTime + ", total_Seat=" + totalSeats + "]";
 	}
-	
-	
-	
 
-	
 }
